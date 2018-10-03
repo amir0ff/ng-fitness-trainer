b2f49a04296c253d64df210b40fc53d7330edf30
Amir Off <ameer157@gmail.com>
2018-10-03 15:45:21 +0300

    Add canActivate guard

--- src/app/training/training-routing.module.ts
+++ src/app/training/training-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
+import { AuthGuard } from '../auth/auth.guard';
 import { TrainingComponent } from './training.component';
 
 const routes: Routes = [
-  { path: '', component: TrainingComponent }
+  { path: '', component: TrainingComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
